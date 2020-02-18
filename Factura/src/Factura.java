00460093865d2b7c3b0bdcdb50382e3e4eb92c9b
agustindelamorena <agustindelamorena01@gmail.com>
2020-02-18 18:47:40 +0100

    cambio a protected en la propiedad estado

--- Factura/src/Factura.java
+++ Factura/src/Factura.java
@@ -3,5 +3,5 @@ public abstract class Factura implements IFactura
     public int num;
     public float base;
     public float tipoIva;
-    public Estados estado;
+    protected Estados estado;
 }
