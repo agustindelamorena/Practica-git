0dc8e53e0577ce8e2f784237e1cd6e3fff4399dd
agustindelamorena <agustindelamorena01@gmail.com>
2020-02-19 17:50:32 +0100

    implementación de métodos

--- Factura/src/Factura.java
+++ Factura/src/Factura.java
@@ -1,7 +1,23 @@
-public abstract class Factura implements IFactura
+public class Factura implements IFactura
 {
     public int num;
     public float base;
     public float tipoIva;
     protected Estados estado;
+
+
+    @Override
+    public void cambiarEstado(Estados estado) {
+
+    }
+
+    @Override
+    public float getTotal() {
+        return 0;
+    }
+
+    @Override
+    public float getCantidadIva() {
+        return 0;
+    }
 }
